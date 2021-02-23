e72b8e0ef3353b624d4f2e3f076b0ee6418fd144
Seamus <seamusugochukwu@gmail.com>
2021-02-23 17:11:04 +0100

    Initial commit of my components without tests

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React, { Component } from "react";
+import Header from "./component/header";
+import "./app.scss";
 
-function App() {
-  return <div className="App">Test</div>;
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
